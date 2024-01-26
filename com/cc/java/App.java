c14c418da0703657fe8e7a27b42f12b532141c52
Peter Schmidt <schmidt.multimedia@gmail.com>
2024-01-26 11:31:45 +0100

    Add files via upload

---
+++ com/cc/java/App.java
@@ -0,0 +1,14 @@
+package com.cc.java;
+
+public class App {
+
+	public static void main(String[] args) {
+	
+	}
+
+
+	private static void output(String outStr) {
+		System.out.println(outStr);
+	}
+
+};
---
+++ com/cc/java/Mitarbeiter.java
@@ -0,0 +1,23 @@
+package com.cc.java;
+
+public class Mitarbeiter {
+	
+	private String familyName ;
+	private String firstName;
+	private String role ;
+	private int yearOfEntry ;
+	
+	public Mitarbeiter(String familyName, String firstName, String role, int yearOfEntry) {
+		this.familyName = familyName;
+		this.firstName = firstName;
+		this.role = role;
+		this.yearOfEntry = yearOfEntry;
+	}
+
+
+	public String getInfo(String flag) {
+		// switch ..
+		return null;
+	}
+
+}
